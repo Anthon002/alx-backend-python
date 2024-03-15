878b0137bcff832485d8c6503a919ccef2e4afc4
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-03-15 02:33:12 +0100

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''This module function to_str.
+'''
+
+
+def to_str(n: float) -> str:
+    '''This method converts float to string
+    '''
+    tostr_ = str(n)
+    return tostr
