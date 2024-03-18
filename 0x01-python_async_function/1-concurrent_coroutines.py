0a9f01b0d421f1d75c1b775c1a19fc2a7d5e2050
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-03-18 20:44:34 +0100

    list of asyncio

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 '''This module contains the implementation of wait_n
 '''
+from typing import List
 import asyncio
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n, max_delay: int) -> float:
-  '''This method calls the wait_n multiple times
-  '''
-  asynclist = []
-  for i in range(n):
-    asynclist.append(await wait_random(max_delay))
-  return asynclist
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''This method calls the wait_n multiple times
+    '''
+    delays = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(delays)
