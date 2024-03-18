189aff225e9e5f4f9dabae9b115b0e505092117e
Anthon002 <chineduanulugwo@gmail.com>
2024-03-18 19:39:24 +0000

    list of async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,7 @@
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n, max_delay: int) -> List[float]:
+async def wait_n(n, max_delay: int) -> float:
   '''This method calls the wait_n multiple times
   '''
   asynclist = []
