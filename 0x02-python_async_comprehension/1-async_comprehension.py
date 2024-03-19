742c19f996d6edb9d7104ffc72b8ff01e01a6aa9
Anthon002 <chineduanulugwo@gmail.com>
2024-03-19 20:41:12 +0000

    async comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,6 +6,9 @@ import asyncio
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension():
+    '''This method uses async list comprehension
+    '''
     async_comp = [i async for i in async_generator()]
     return async_comp
